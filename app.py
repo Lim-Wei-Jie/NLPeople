0355ad82aed7d1a67c56690c42858ba4032ac2e4
r1nald98 <101116830+r1nald98@users.noreply.github.com>
2023-01-20 16:56:30 +0800

    Update app.py
    Normal Texts

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-Hello Ronald
+"""
+# Welcome to Streamlit! Hello Ronald here
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
