94d2f7da2047c273889734548626396da9b1adea
RickyGoh <89062463+RickyGoh@users.noreply.github.com>
2023-01-26 21:47:01 +0800

    test extraction

---
+++ testextract.py
@@ -0,0 +1,15 @@
+import os
+import tabula
+import requests
+import re
+from urllib.parse import parse_qsl, urljoin, urlparse
+import os
+
+path_of_the_directory= 'C:/Users/arkgn/Desktop/GG1'
+print("Files and directories in a specified path:")
+for filename in os.listdir(path_of_the_directory):
+    f = os.path.join(path_of_the_directory,filename)
+    if os.path.isfile(f):
+        print(f)
+        tables = tabula.read_pdf(b, pages="all", multiple_tables=True,encoding='cp1252')
+        print(tables)
