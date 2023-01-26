49b950b3d110e4e007867c3b8f4fa12606c62ab1
RickyGoh <89062463+RickyGoh@users.noreply.github.com>
2023-01-26 22:07:51 +0800

    update to testextract.py
    convert to csv but unable to convert to xlsx

--- testextract.py
+++ testextract.py
@@ -4,6 +4,7 @@ import requests
 import re
 from urllib.parse import parse_qsl, urljoin, urlparse
 import os
+import pandas as pd
 
 path_of_the_directory= 'C:/Users/arkgn/Desktop/GG1'
 print("Files and directories in a specified path:")
@@ -12,4 +13,6 @@ for filename in os.listdir(path_of_the_directory):
     if os.path.isfile(f):
         print(f)
         tables = tabula.read_pdf(f, pages="all", multiple_tables=True,encoding='cp1252')
+        filename = f.replace(".pdf", "")
+        tabula.convert_into(f, '{}.csv'.format(filename), pages='all')
         print(tables)
