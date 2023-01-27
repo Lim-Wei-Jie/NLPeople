ccda3ab1f3b9ed6519312aabbf9ff328f82932ac
Lim-Wei-Jie <weijie.lim.2020@scis.smu.edu.sg>
2023-01-27 13:30:24 +0800

    update

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import pandas as pd
 # import camelot as cam
 # import numpy as np
 # import matplotlib.pyplot as plt
+
 ################################################
 
 def _max_width_():
@@ -42,66 +43,68 @@ with c30:
     )
 
     if uploaded_file is not None:
-        # file_container = st.expander("Check your uploaded .csv")
-        shows = pd.read_pdf(uploaded_file)
-        uploaded_file.seek(0)
+        df = tabula.read_pdf(uploaded_file, pages="all", multiple_tables=True, encoding='cp1252')
+        print(df)
+        # file_container = st.expander("Check your uploaded .pdf")
+        # shows = pd.read_pdf(uploaded_file)
+        # uploaded_file.seek(0)
         # file_container.write(shows)
 
-    # else:
-        # st.info(
-        #     f"""
-        #         👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
-        #         """
-        # )
+    else:
+        st.info(
+            f"""
+                👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
+                """
+        )
 
-        # st.stop()
+        st.stop()
 
-from st_aggrid import GridUpdateMode, DataReturnMode
+# from st_aggrid import GridUpdateMode, DataReturnMode
 
-gb = GridOptionsBuilder.from_dataframe(shows)
-# enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
-gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
-gb.configure_selection(selection_mode="multiple", use_checkbox=True)
-gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
-gridOptions = gb.build()
+# gb = GridOptionsBuilder.from_dataframe(shows)
+# # enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
+# gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
+# gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+# gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
+# gridOptions = gb.build()
 
-st.success(
-    f"""
-        💡 Tip! Hold the shift key when selecting rows to select multiple rows at once!
-        """
-)
+# st.success(
+#     f"""
+#         💡 Tip! Hold the shift key when selecting rows to select multiple rows at once!
+#         """
+# )
 
-response = AgGrid(
-    shows,
-    gridOptions=gridOptions,
-    enable_enterprise_modules=True,
-    update_mode=GridUpdateMode.MODEL_CHANGED,
-    data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-    fit_columns_on_grid_load=False,
-)
+# response = AgGrid(
+#     shows,
+#     gridOptions=gridOptions,
+#     enable_enterprise_modules=True,
+#     update_mode=GridUpdateMode.MODEL_CHANGED,
+#     data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+#     fit_columns_on_grid_load=False,
+# )
 
-df = pd.DataFrame(response["selected_rows"])
+# df = pd.DataFrame(response["selected_rows"])
 
-st.subheader("Filtered data will appear below 👇 ")
-st.text("")
+# st.subheader("Filtered data will appear below 👇 ")
+# st.text("")
 
-st.table(df)
+# st.table(df)
 
-st.text("")
+# st.text("")
 
-c29, c30, c31 = st.columns([1, 1, 2])
+# c29, c30, c31 = st.columns([1, 1, 2])
 
-with c29:
+# with c29:
 
-    CSVButton = download_button(
-        df,
-        "File.csv",
-        "Download to CSV",
-    )
+#     CSVButton = download_button(
+#         df,
+#         "File.csv",
+#         "Download to CSV",
+#     )
 
-with c30:
-    CSVButton = download_button(
-        df,
-        "File.csv",
-        "Download to TXT",
-    )
+# with c30:
+#     CSVButton = download_button(
+#         df,
+#         "File.csv",
+#         "Download to TXT",
+#     )
--- testextract.py
+++ testextract.py
@@ -6,13 +6,13 @@ from urllib.parse import parse_qsl, urljoin, urlparse
 import os
 import pandas as pd
 
-path_of_the_directory= 'C:/Users/arkgn/Desktop/GG1'
+path_of_the_directory= 'C:/Users/lim_w/FYP/sample-financial-statements/public/nikon'
 print("Files and directories in a specified path:")
 for filename in os.listdir(path_of_the_directory):
     f = os.path.join(path_of_the_directory,filename)
     if os.path.isfile(f):
         print(f)
         tables = tabula.read_pdf(f, pages="all", multiple_tables=True,encoding='cp1252')
-        filename = f.replace(".pdf", "")
-        tabula.convert_into(f, '{}.csv'.format(filename), pages='all')
+        # filename = f.replace(".pdf", "")
+        # tabula.convert_into(f, '{}.csv'.format(filename), pages='all')
         print(tables)
