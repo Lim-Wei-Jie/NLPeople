a4041e6eeca65c2e8aa0a7725b445851789d112e
RickyGoh <89062463+RickyGoh@users.noreply.github.com>
2023-01-29 23:26:07 +0800

    tabular attempt 2

--- testextract.py
+++ testextract.py
@@ -6,13 +6,17 @@ from urllib.parse import parse_qsl, urljoin, urlparse
 import os
 import pandas as pd
 
-path_of_the_directory= 'C:/Users/lim_w/FYP/sample-financial-statements/public/nikon'
+path_of_the_directory= 'C:/Users/arkgn/Desktop/GG1'
 print("Files and directories in a specified path:")
 for filename in os.listdir(path_of_the_directory):
     f = os.path.join(path_of_the_directory,filename)
     if os.path.isfile(f):
         print(f)
-        tables = tabula.read_pdf(f, pages="all", multiple_tables=True,encoding='cp1252')
-        # filename = f.replace(".pdf", "")
-        # tabula.convert_into(f, '{}.csv'.format(filename), pages='all')
-        print(tables)
+        dfs = tabula.read_pdf(f, pages='all',stream=True)
+        print(len(dfs))
+        for i in range(len(dfs)):
+            print(dfs[i])
+     
+
+        
+     
