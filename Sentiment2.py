cee2abbce057394ddbdc9291aa78d01cd75c367f
RickyGoh <89062463+RickyGoh@users.noreply.github.com>
2023-03-07 22:18:35 +0800

    version1
    using fibert + spacy

--- Sentiment2.py
+++ Sentiment2.py
@@ -16,8 +16,8 @@ import spacy
 import matplotlib.pyplot as plt
 import os
 
-text = extract_text('input.pdf')
-print(text)
+text = extract_text(r'C:\Users\arkgn\Desktop\public\grab\Grab Q2 22.pdf')
+#print(text)
 
 
 nlp_spacy = spacy.load("en_core_web_sm")
@@ -25,7 +25,55 @@ doc = nlp_spacy(text)
 
 sentences_annual_report = []
 for sent in doc.sents:
-    if len(sent.text.split()) > 3:
+    if len(sent.text.split()) > 6:
+        print(sent.text)
         sentences_annual_report.append(sent.text)
     
-print(sentences_annual_report)
+#print(sentences_annual_report)
+
+
+finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone',num_labels=3)
+tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+
+nlp = pipeline("sentiment-analysis", model=finbert, tokenizer=tokenizer)
+
+results = nlp(sentences_annual_report)
+
+
+
+sentiment = pd.DataFrame({"docs": sentences_annual_report,
+                          "label": [r["label"] for r in results],
+                          "score":[r["score"] for r in results],
+                          "docs": sentences_annual_report})
+
+
+
+
+Positive = 0
+Negative = 0
+Neutral = 0
+
+#print(sentiment.label)
+    
+    #if x.label == "Positive":
+    #    Positive += 1
+    
+#print("Positive: " + Positive)
+
+for x,y,z in zip(sentiment.label,sentiment.docs,sentiment.score):
+    if x == "Positive":
+        Positive += 1
+        print(y)
+        print()
+       
+        
+    if x == "Negative":
+        Negative += 1
+        
+        
+    if x == "Neutral":
+        Neutral += 1
+
+print("Positive :" + str(Positive))
+print("Negative :" + str(Negative))
+print("Neutral :" + str(Neutral))
